3390267f8018e8c9310df6180b631e63d40c1c1a
Bryan Cohen <82907967+bryantqr@users.noreply.github.com>
2023-04-05 12:55:34 -0400

    adding tests to job validator class

--- mirrulations-validation/tests/test_job_validator.py
+++ mirrulations-validation/tests/test_job_validator.py
@@ -13,10 +13,3 @@ def test_download(self):
     # Assert that all jobs have been validated
     self.assertEqual(validator.counter['Total_validated'], storage.get_collection_size(endpoint))
 
-def test_write_unfound_jobs(self):
-    # Test the write_unfound_jobs function
-    unfound_jobs = {}
-    res = {'id': 1, 'type': 'docket', 'links': {'self': 'http://example.com/docket/1'}}
-    write_unfound_jobs(res, unfound_jobs)
-    # Assert that missing_docket contains the URL
-    self.assertIn('http://example.com/docket/1', unfound_jobs['missing_docket'])
